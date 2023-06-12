5f873239ef08232ae14e382d9f0df0c452e27059
Shrujan <shrork@gmail.com>
2023-06-12 17:34:03 +0530

    skills

--- src/Component/Skills/Skills.tsx
+++ src/Component/Skills/Skills.tsx
@@ -1,6 +1,15 @@
+import styles from './Skills.module.scss'
 
 const Skills = () => {
-  return <section>
+  return <section className={ styles['skills-section'] }>
+    <div>Angular</div>
+    <div>React</div>
+    <div>JavaScript</div>
+    <div>TypeScript</div>
+    <div>HTML</div>
+    <div>SCSS</div>
+    <div>Data Structures</div>
+    <div>Git</div>
   </section>
 }
 
